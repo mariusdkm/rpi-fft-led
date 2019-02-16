bbf9b63c1a5e363074f8afe4630dffb6b7e288bd
mariusdkm <43829743+mariusdkm@users.noreply.github.com>
2019-02-16 18:35:06 +0100

    Added microphone support

--- fft.c
+++ fft.c
@@ -12,6 +12,9 @@
 #include <sys/ioctl.h>
 #include <linux/types.h>
 
+//to get the maximum number
+#include <limits.h>
+
 // for reading wav files:
 #include <sndfile.h>
 
@@ -28,6 +31,38 @@
 
 
 int main(int argc, char *argv[]) {
+	
+	int NLOG2 = 12;
+	unsigned long N = 1 << NLOG2;
+	//loop is used to control how often the analyzing is going
+	int loop = INT_MAX;
+	//Read arguments
+	int mode = 2;
+	bool mic = false;
+	bool optimize = false;
+	if(!strcmp(argv[1], "mic"))
+	{
+		mic = true;
+		printf("microphone enabled\n");
+	} else {
+		printf("soundfile enabled\n");
+	}
+	if (argc > 2)
+	{
+		mode = (int) (*argv[2]-'1');
+	}
+	if (argc > 3)
+	{
+	optimize = true;
+	printf("optimizing enabled\n");
+	}
+	
+	printf("mode %d enabled\n",mode);
+	
+	//Open the WAV file.
+	if (argc < 2 || argc > 4)
+		pabort("Call me with the name of the sound file as argument or mic if you want to use a microphone and if you want a second argument for the LED mode (and optionally a third  argument to optimize display for this sound file)");
+	
 	//ALSA audio device init-------------------
 	int err;
 	int16_t *buffer;
@@ -40,9 +75,13 @@ int main(int argc, char *argv[]) {
 	snd_pcm_uframes_t frames;
 	snd_pcm_format_t format = SND_PCM_FORMAT_S16_LE;
 	
-	/* Open PCM device for playback. */
-	rc = snd_pcm_open(&handle, "default",
-					  SND_PCM_STREAM_PLAYBACK, 0);
+	if(mic)
+		/* Open PCM device for playback. */
+		rc = snd_pcm_open(&handle, "hw:1,0", SND_PCM_STREAM_CAPTURE, 0);
+	else
+		/* Open PCM device for playback. */
+		rc = snd_pcm_open(&handle, "default", SND_PCM_STREAM_PLAYBACK, 0);
+
 	if (rc < 0) {
 		fprintf(stderr,
 				"unable to open pcm device: %s\n",
@@ -72,7 +111,7 @@ int main(int argc, char *argv[]) {
 	snd_pcm_hw_params_set_rate_near(handle, params, &val, &dir);
 	
 	/* Set period size to 32 frames. */
-	frames = 1024;
+	frames = N;
 	snd_pcm_hw_params_set_period_size_near(handle, params, &frames, &dir);
 	
 	/* Write the parameters to the driver */
@@ -90,56 +129,44 @@ int main(int argc, char *argv[]) {
 	
 	//allocate buffer of 16bit ints, as specified in PCM_FORMAT
 	buffer = malloc(frames * snd_pcm_format_width(format) / 8 * 2);
+
 	//File reading code-----------------------
-	SNDFILE *sf;
-	SF_INFO sf_info;
-	int num_channels, num, num_items;
 	short *buf;
-	FILE *out;
-	bool optimize = false;
+	if(!mic){
+		SNDFILE *sf;
+		SF_INFO sf_info;
+		int num_items;
+		FILE *out;
+		
+		sf_info.format = 0;
+		sf = sf_open(argv[1],SFM_READ,&sf_info);
+		if (sf == NULL)
+		{
+		printf("Failed to open the file.\n");
+		exit(-1);
+		}
+		//Print some of the sf_info, and figure out how much data to read.
+		printf("frames=%d\n",sf_info.frames);
+		printf("samplerate=%d\n",sf_info.samplerate);
+		printf("channels=%d\n",sf_info.channels);
+		printf("format=%d\n",sf_info.format);
+		printf("sections=%d\n",sf_info.sections);
+		printf("seekable=%d\n",sf_info.seekable);
 	
-	int num_bins,current_bin,current_led;
-	float max,avg,amplitude,max_amplitude = 0,amplitude_factor,freq_scaling;
-	uint32_t spi_speed = 4000000; //=250ns
-	
-	if (argc > 2)
-	{
-		optimize = true;
-		printf("optimizing enabled\n");
+		num_items = sf_info.frames*sf_info.channels;
+		printf("num_items=%d\n",num_items);
+		//Allocate space for the data to be read, then read it.
+		buf = (short *) malloc(num_items*sizeof(short));
+		printf("Read %d items\n", sf_read_short(sf,buf,num_items));
+		sf_close(sf);
+		// code below only works for mono files, so abort here if not mono
+		if (sf_info.channels > 1)
+			pabort("Please use a mono wav file, not stereo.");
+		
+		loop = sf_info.frames/N;
 	}
-	
-	 //Open the WAV file.
-	 if (argc < 2 || argc > 3)
-		 pabort("Call me with the name of the sound file as argument (and optionally a second argument to optimize display for this sound file)");
-	
-	 
-	 sf_info.format = 0;
-	 sf = sf_open(argv[1],SFM_READ,&sf_info);
-	 if (sf == NULL)
-	 {
-	 printf("Failed to open the file.\n");
-	 exit(-1);
-	 }
-	 //Print some of the sf_info, and figure out how much data to read.
-	 printf("frames=%d\n",sf_info.frames);
-	 printf("samplerate=%d\n",sf_info.samplerate);
-	 printf("channels=%d\n",sf_info.channels);
-	 printf("format=%d\n",sf_info.format);
-	 printf("sections=%d\n",sf_info.sections);
-	 printf("seekable=%d\n",sf_info.seekable);
-	 
-	 num_items = sf_info.frames*sf_info.channels;
-	 printf("num_items=%d\n",num_items);
-	 //Allocate space for the data to be read, then read it.
-	 buf = (short *) malloc(num_items*sizeof(short));
-	 num = sf_read_short(sf,buf,num_items);
-	 sf_close(sf);
-	 printf("Read %d items\n",num);
-	 
-	 // code below only works for mono files, so abort here if not mono
-	 if (sf_info.channels > 1)
-	 pabort("Please use a mono wav file, not stereo.");
 	// -------------------------------------------------------------------
+	
 	
 	
 	int ret = 0;
@@ -148,74 +175,97 @@ int main(int argc, char *argv[]) {
 	
 	//FFT Variables
 	float * OutData;
-	int jobs=1, c, mb = mbox_open(), NLOG2 = 10;
+	int jobs=1, c, mb = mbox_open();
 	double r,g,b;
 	
 	struct GPU_FFT_COMPLEX * base;
 	struct GPU_FFT * fft;
 	struct GPU_FFT_COMPLEX *DataIn, *DataOut;
-	unsigned long N = 1 << NLOG2;
+	
 	float FloatN = (float) N;
 	float HalfN  = FloatN / 2.0;
-	unsigned int rate = 44100;
+	int rate = 44100;
 	
-	// a structure encoding the rgb value for all of the leds in the strip
-	struct led_data ledstrip_data[num_leds];
-	//init spi_led.c
-	fd = init_spi_led(spi_speed);
-	
-	
-	OutData = malloc(N/2 * sizeof(float));
-	ret = gpu_fft_prepare(mb, NLOG2, GPU_FFT_FWD, jobs, &fft);
-	bool logarithmic = false;
 	//low for how many freq are used for the base, and high how many are used for the unused high parts
 	int low, high, brightness;
-	low = 300;
-	high = 3000;
-	brightness = 100;
-	int freq_per_bin = rate/N;
+	low = 100;
+	high = 1000;
+	brightness = 70;
+	float freq_per_bin=rate*1.0/N;
+	
 	int low_bins, high_bins;
 	
 	//get how many bin are in the low part
 	low_bins = round(low/freq_per_bin);
-	
 	//get how many bin are in the hight part
-	high_bins = round((rate/2 - high)/freq_per_bin);
+	high_bins = round((rate/2-high)/freq_per_bin);
 	
-	for(int e=0; e<sf_info.frames/N;e++){
+	// a structure encoding the rgb value for all of the leds in the strip
+	struct led_data ledstrip_data[num_leds];
+	
+	uint32_t spi_speed = 4000000; //=250ns
+	float max_amplitude = 0,amplitude_factor;
+	//init spi_led.c-----------------------
+	fd = init_spi_led(spi_speed);
+	
+	//set all LED to black(0,0,0) to start
+	led_data led_zero = {
+		.r = 0,
+		.g = 0,
+		.b = 0
+	};
+	for(i=0; i<num_leds; i++)
+		ledstrip_data[i]=led_zero;
+	
+	write_spi_buffer(ledstrip_data, fd);
+	
+	typedef void (*led_mode)(unsigned long N, float * Data, int low_bins, int high_bins, int brightness, float amplitude_factor, bool logarithmic, led_data (*ledstrip_data)[]);
+	
+	led_mode led_modes[4] = {&mode1, &mode2, &mode3 , &mode4};
+	
+	OutData = malloc(N/2 * sizeof(float));
+	ret = gpu_fft_prepare(mb, NLOG2, GPU_FFT_FWD, jobs, &fft);
+	bool logarithmic = false;
+	for(int e=0; e<loop;e++){
 		base = fft->in;
 		
-		//read from audio device into buffer
-		/*if ((err = snd_pcm_readi(capture_handle, buffer, buffer_frames)) != buffer_frames) {
-			fprintf(stderr, "read from audio interface failed (%s)\n",
-					err, snd_strerror(err));
-			exit(1);
-		}*/
+		if(mic){
+			//read from audio device into buffer
+			if ((err = snd_pcm_readi(handle, buffer, frames)) != frames) {
+				fprintf(stderr, "read from audio interface failed (%s)\n",
+						err, snd_strerror(err));
+				exit(1);
+			}
+		}
 		
 		//Write the Data into the FFT Buffer
 		for(i=0;i<N;i++){
-			base[i].re = buf[i+e*N]/32768.0;
 			base[i].im = 0.0;
-			
-			//Buffer for the playing the sound
-			buffer[i] = buf[i+e*N];
+			if(mic){
+				base[i].re = buffer[i]/32768.0;
+			}else{
+				base[i].re = buf[i+e*N]/32768.0;
+				//Buffer for the playing the sound
+				buffer[i] = buf[i+e*N];
+			}
+
 		}
-		
-		//play the buffer
-		rc = snd_pcm_writei(handle, buffer , frames);
-		if (rc == -EPIPE) {
-			/* EPIPE means underrun */
-			fprintf(stderr, "underrun occurred\n");
-			snd_pcm_prepare(handle);
-		} else if (rc < 0) {
-			fprintf(stderr,
-					"error from writei: %s\n",
-					snd_strerror(rc));
-		}  else if (rc != (int)frames) {
-			fprintf(stderr,
-					"short write, write %d frames\n", rc);
+		if(!mic){
+			//play the buffer
+			rc = snd_pcm_writei(handle, buffer , frames);
+			if (rc == -EPIPE) {
+				// EPIPE means underrun
+				fprintf(stderr, "underrun occurred\n");
+				snd_pcm_prepare(handle);
+			} else if (rc < 0) {
+				fprintf(stderr,
+						"error from writei: %s\n",
+						snd_strerror(rc));
+			}  else if (rc != (int)frames) {
+				fprintf(stderr,
+						"short write, write %d frames\n", rc);
+			}
 		}
-		
 		//Perform FFT
 		gpu_fft_execute(fft);
 		
@@ -241,7 +291,7 @@ int main(int argc, char *argv[]) {
 			amplitude_factor = 1.0;
 		
 		//Calculate the LEDs using one of the modes in led_modes
-		mode1(N, OutData, low_bins, high_bins,  brightness, amplitude_factor, logarithmic, &ledstrip_data);
+		led_modes[mode](N, OutData, low_bins, high_bins,  brightness, amplitude_factor, logarithmic, &ledstrip_data);
 		//write the LED data to the LEDs
 		write_spi_buffer(ledstrip_data, fd);
 	}
